aa6fdedaee272f62dbf2d898756c0ad96fef5546
Muhtasim-Ul-Alam <muhtasimalam19@gmail.com>
2022-05-19 20:02:05 +0600

    deploy

--- src/main/java/com/bjit/recruitment/RecruitmentApplication.java
+++ src/main/java/com/bjit/recruitment/RecruitmentApplication.java
@@ -2,9 +2,9 @@ package com.bjit.recruitment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+
+
 @SpringBootApplication
 public class RecruitmentApplication {
 
