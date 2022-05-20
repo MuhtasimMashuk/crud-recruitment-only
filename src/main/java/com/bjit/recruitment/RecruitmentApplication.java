950e267b81992d8fcec49daa71f10c79e21cfe26
Muhtasim-Ul-Alam <muhtasimalam19@gmail.com>
2022-05-20 08:38:10 +0600

    swagger added ,various changes including yml and application file

--- src/main/java/com/bjit/recruitment/RecruitmentApplication.java
+++ src/main/java/com/bjit/recruitment/RecruitmentApplication.java
@@ -2,14 +2,43 @@ package com.bjit.recruitment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableWebMvc
 public class RecruitmentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RecruitmentApplication.class, args);
+
+    }
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.bjit.recruitment"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+
+        return new ApiInfoBuilder()
+                .title("Crud Operation API Doc")
+                .description("for the assignment from BJIT")
+                .version("1.0.0")
+                .build();
     }
 
 }
